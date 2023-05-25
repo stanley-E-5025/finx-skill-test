29db926a58c5b8fb7f8504c8ffcd23a979db954b
stanley-E-5025 <stanlyumana@Stanlys-MacBook-Air.local>
2023-05-25 11:23:23 -0600

    Setup: Configured multi-database support and added migrations for both primary and secondary databases.

--- core_app/settings.py
+++ core_app/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'secondary': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'secondary.sqlite3',
     }
 }
 
--- multi_db_query/views.py
+++ multi_db_query/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
 def home_page(request):
-    pass
+    return HttpResponse("Welcome")
+
+def user_details(request, name):
+    return HttpResponse(f"<h1>hello {name}</h1>")
